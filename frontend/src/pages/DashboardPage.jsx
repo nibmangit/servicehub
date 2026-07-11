9e3c47bcbd9770b6633b22a3cff98cd475469b2e
nibmangit <nibretumengaw@gmail.com>
2026-07-11 16:59:39 +0300

    feat: add dashboard feature

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,3 +1,46 @@
+import DashboardStats from "../components/dashboard/DashboardStats";
+import RecentRequests from "../components/dashboard/RecentRequests";
+import RecentReviews from "../components/dashboard/RecentReviews";
+import RecentChats from "../components/dashboard/RecentChats";
+
 export default function DashboardPage() {
-    return <h1>Dashboard</h1>;
+
+    return (
+
+        <div className="space-y-10">
+
+            <div>
+
+                <h1 className="text-3xl font-bold">
+                    Dashboard
+                </h1>
+
+                <p className="text-slate-500 mt-2">
+                    Welcome back, Nibretu 👋
+                </p>
+
+            </div>
+
+            <DashboardStats />
+
+            <div
+                className="
+                    grid
+                    gap-6
+                    lg:grid-cols-2
+                "
+            >
+
+                <RecentRequests />
+
+                <RecentReviews />
+
+            </div>
+
+            <RecentChats />
+
+        </div>
+
+    );
+
 }
